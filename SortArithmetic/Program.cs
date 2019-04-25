d3248c42f068106c02397d45fe0fdda8c132b2a4
huangchuang <1838415801@qq.com>
2019-04-25 20:52:28 +0800

    添加冒泡和选择排序算法

---
+++ SortArithmetic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = {2,10,8,1,4,3,6,5,11 };
+            //Sort.BubbleSort(arry);
+            Sort.SelectionSort(arry);
+            for (int i=0;i<arry.Length;i++)
+            {
+                Console.WriteLine(arry[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortArithmetic/Sort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArithmetic
+{
+    class Sort
+    {
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="arrayVal"></param>
+        public static void BubbleSort(int[] arrayVal)
+        {
+            //极值的置尾
+            //for (int i=0;i<arrayVal.Length;i++)
+            //{
+            //    for (int j= 0; j < arrayVal.Length - 1-i;j++)
+            //    {
+            //        if (arrayVal[j]>arrayVal[j+1])
+            //        {
+            //            int temp = arrayVal[j];
+            //            arrayVal[j] = arrayVal[j+1];
+            //            arrayVal[j+1] = temp;
+            //        }
+            //    }
+            //}
+            //极值的置顶
+            for (int i = 0; i < arrayVal.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arrayVal.Length; j++)
+                {
+                    if (arrayVal[i] > arrayVal[j])
+                    {
+                        int temp = arrayVal[i];
+                        arrayVal[i] = arrayVal[j];
+                        arrayVal[j] = temp;
+                    }
+                }                    
+            }               
+        }
+        /// <summary>
+        /// 选择排序
+        /// </summary>
+        /// <param name="arrayVal"></param>
+        public static void SelectionSort(int[] arrayVal)
+        {
+            for (int i=0;i<arrayVal.Length;i++)
+            {
+                int keyindex = i;
+                for (int j=keyindex+1;j<arrayVal.Length;j++)
+                {
+                    if (arrayVal[j]<arrayVal[keyindex])
+                    {
+                        keyindex = j;
+                    }
+                }
+                int temp = arrayVal[i];
+                arrayVal[i] = arrayVal[keyindex];
+                arrayVal[keyindex] = temp;
+            }
+
+        }
+    }
+}
